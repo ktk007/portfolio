ead28f9a07b72caf1dc527e9af667802ff941b24
Khadijatull Kobra <85698460+ktk007@users.noreply.github.com>
2024-01-29 10:33:47 +0600

    Add files via upload
    portfolio

---
+++ scriptp.js
@@ -0,0 +1,14 @@
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Set current year in the footer
+const currentYear = new Date().getFullYear();
+document.getElementById('footer-year').innerText = currentYear;
